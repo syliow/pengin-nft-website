674d3aa702105838363d29fa9c2cd7b910671f71
Shanyi Liow <shanyiliow1107@gmail.com>
2022-05-11 11:34:56 +0800

    chore: modified about us section

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -4,8 +4,8 @@ import config from "../config/index.json";
 import Divider from "./Divider";
 
 const Product = () => {
-  const { product } = config;
-  const [firstItem, secondItem] = product.items;
+  const { aboutUs } = config;
+  const [firstItem, secondItem] = aboutUs.items;
 
   return (
     <section className={`bg-background py-8`} id="product">
@@ -13,7 +13,7 @@ const Product = () => {
         <h1
           className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
         >
-          {product.title.split(" ").map((word, index) => (
+          {aboutUs.title.split(" ").map((word, index) => (
             <span
               key={index}
               className={index % 2 ? "text-primary" : "text-secondary"}
@@ -70,7 +70,7 @@ const Product = () => {
             <div className="p-5 sm:mt-0 sm:ml-3">
               <a
                 href={"https://testnets.opensea.io/collection/pengin-nft"}
-                className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10 text-center`}
+                className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary  text-secondary bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10 text-center`}
               >
                 {/* {mainHero.secondaryAction.text} */}
                 Buy on OpenSea
