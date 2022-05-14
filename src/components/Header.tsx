86250b7403406f8d479de71bae45a5659d0a5fac
Shanyi Liow <shanyiliow1107@gmail.com>
2022-05-14 23:39:29 +0800

    chore: modify text to fit with theme color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,7 +53,7 @@ const Menu = () => {
                   duration={1000}
                   key={item.name}
                   to={item.href}
-                  className="font-medium text-gray-500 hover:text-gray-900"
+                  className="font-medium text-white hover:text-primary"
                 >
                   {item.name}
                 </Link>
