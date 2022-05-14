53bc9101fb83f96640c67e025e6cdf395c2df201
Shanyi Liow <shanyiliow1107@gmail.com>
2022-05-14 23:41:27 +0800

    chore: minor changes for footer section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import config from "../config/index.json";
 
 const About = () => {
-  const { company, about } = config;
+  const { company, aboutMe } = config;
   const { logo, name: companyName } = company;
-  const { socialMedia, sections } = about;
+  const { socialMedia } = aboutMe;
 
   return (
     <div
@@ -15,17 +15,6 @@ const About = () => {
       <div className="flex flex-col items-center justify-center">
         <div>
           <img src={logo} alt={companyName} className="w-16 h-16" />
-        </div>
-        <div className="flex flex-wrap sm:gap-10 gap-8 items-center justify-center mt-4 h-12">
-          {sections.map((section, index) => (
-            <a
-              key={`${section.name}-${index}`}
-              href={section.href}
-              className="hover:text-primary text-base cursor-pointer leading-4 text-white dark:text-gray-400 dark:hover:text-white"
-            >
-              {section.name}
-            </a>
-          ))}
         </div>
         <div className="flex items-center gap-x-8 mt-6 h-8">
           <a
@@ -64,9 +53,9 @@ const About = () => {
         <div className="flex items-center mt-6">
           <p className="mt-6 text-xs lg:text-sm leading-none text-white">
             Made with ❤️ by {""}
-            <a href="https://github.com/syliow" rel="nofollow">
-              Shanyi Liow
-            </a>
+            {/* <a href="https://github.com/syliow" rel="nofollow"> */}
+            Shanyi Liow
+            {/* </a> */}
           </p>
         </div>
       </div>
